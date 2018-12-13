48892cb4aefb9108ce16c126737d3643ccfeae43
Mentor Palokaj <mentor@palokaj.co>
2018-12-13 18:55:33 +0100

    Coockified

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,54 @@
+import Cookie from 'js-cookie'
 const date = new Date()
 const hour = date.getHours()
+const midnight = new Date( ).setHours(24,0,0,0)
 
 const q = query => document.querySelector( query )
+const qa = query => document.querySelectorAll( query )
 const trans = query => q( query ).classList.add( 'trans' )
 const untrans = query => q( query ).classList.remove( 'trans' )
 
 // Before 12 the pug already does the relevant trans
 // if( hour < 12 ) 
-if( hour > 17 ) {
-	untrans( '#evening' )
-	trans( '#wake' )
-	trans( '#plan' )
+// if( hour > 17 ) {
+// 	untrans( '#evening' )
+// 	trans( '#wake' )
+// 	trans( '#plan' )
+// }
+
+window.onload = f => {
+
+	// Set input width on change
+	q( "input[type='text']" ).addEventListener( 'keyup', event => event.target.style.width = `${ event.target.value.length }rem` )
+
+	// Get MIT cookie, if present, populate input
+	Promise.resolve( Cookie.get( 'mit' ) )
+	.then( mit => q( '#mit' ).value = mit || 'Most Important Todo...')
+	// If MIT input changes, set cookie
+	q( '#mit' ).addEventListener( 'keyup', event => Cookie.set( 'mit', event.target.value, { expires: midnight } ) )
+
+	// Get LIT cookie, if present, populate input
+	Promise.resolve( Cookie.get( 'lit' ) )
+	.then( lit => q( '#lit' ).value = lit || 'Less Important Todo...')
+	// If LIT input changes, set cookie
+	q( '#lit' ).addEventListener( 'keyup', event => Cookie.set( 'lit', event.target.value, { expires: midnight } ) )
+
+	// Tag clicked todos
+	const habits = qa( '.habits li' )
+	for (let i = habits.length - 1; i >= 0; i--) {
+
+		// Set id
+		habits[i].id = `habit${i}`
+		// Set listener
+		habits[i].addEventListener( 'click', event => {
+			event.target.classList.toggle( 'strike' )
+			console.log( `Set habit${i} to ${event.target.classList.contains( 'strike' )}` )
+			Cookie.set( `habit${i}`, event.target.classList.contains( 'strike' ), { expires: midnight } )
+		} )
+		// Get previous tag states
+		if( Cookie.get( `habit${i}` ) == 'true' ) habits[i].classList.toggle( 'strike' )
+	}
+
+
 }
+
