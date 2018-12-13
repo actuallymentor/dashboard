959856a8389776224292d13017814bb83afea184
Mentor Palokaj <mentor@palokaj.co>
2018-12-13 11:30:36 +0100

    First iteration

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log( 'I am alive' )
+const date = new Date()
+const hour = date.getHours()
+
+const q = query => document.querySelector( query )
+const trans = query => q( query ).classList.add( 'trans' )
+const untrans = query => q( query ).classList.remove( 'trans' )
+
+// Before 12 the pug already does the relevant trans
+// if( hour < 12 ) 
+if( hour > 17 ) {
+	untrans( '#evening' )
+	trans( '#wake' )
+	trans( '#plan' )
+}
