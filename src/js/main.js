f92229afb53feba701b50066c304381787e66cd7
Mentor Palokaj <mentor@palokaj.co>
2018-12-14 11:54:07 +0100

    Remove cookies

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,33 @@
 import Cookie from 'js-cookie'
 const date = new Date()
 const hour = date.getHours()
-const midnight = new Date( ).setHours(24,0,0,0)
+// const midnight = new Date( ).setHours(24,0,0,0)
+// { expires: midnight }
 
 const q = query => document.querySelector( query )
 const qa = query => document.querySelectorAll( query )
 const trans = query => q( query ).classList.add( 'trans' )
 const untrans = query => q( query ).classList.remove( 'trans' )
 
-// Before 12 the pug already does the relevant trans
-// if( hour < 12 ) 
-// if( hour > 17 ) {
-// 	untrans( '#evening' )
-// 	trans( '#wake' )
-// 	trans( '#plan' )
-// }
-
 window.onload = f => {
 
 	// Set input width on change
-	q( "input[type='text']" ).addEventListener( 'keyup', event => event.target.style.width = `${ event.target.value.length }rem` )
+	const inputs = qa( "input[type='text']" )
+	for (var i = inputs.length - 1; i >= 0; i--) {
+		inputs[i].addEventListener( 'keyup', event => event.target.style.width = `${ event.target.value.length }rem` )
+	}
 
 	// Get MIT cookie, if present, populate input
 	Promise.resolve( Cookie.get( 'mit' ) )
 	.then( mit => q( '#mit' ).value = mit || 'Most Important Todo...')
 	// If MIT input changes, set cookie
-	q( '#mit' ).addEventListener( 'keyup', event => Cookie.set( 'mit', event.target.value, { expires: midnight } ) )
+	q( '#mit' ).addEventListener( 'keyup', event => Cookie.set( 'mit', event.target.value ) )
 
 	// Get LIT cookie, if present, populate input
 	Promise.resolve( Cookie.get( 'lit' ) )
 	.then( lit => q( '#lit' ).value = lit || 'Less Important Todo...')
 	// If LIT input changes, set cookie
-	q( '#lit' ).addEventListener( 'keyup', event => Cookie.set( 'lit', event.target.value, { expires: midnight } ) )
+	q( '#lit' ).addEventListener( 'keyup', event => Cookie.set( 'lit', event.target.value ) )
 
 	// Tag clicked todos
 	const habits = qa( '.habits li' )
@@ -43,7 +39,7 @@ window.onload = f => {
 		habits[i].addEventListener( 'click', event => {
 			event.target.classList.toggle( 'strike' )
 			console.log( `Set habit${i} to ${event.target.classList.contains( 'strike' )}` )
-			Cookie.set( `habit${i}`, event.target.classList.contains( 'strike' ), { expires: midnight } )
+			Cookie.set( `habit${i}`, event.target.classList.contains( 'strike' ) )
 		} )
 		// Get previous tag states
 		if( Cookie.get( `habit${i}` ) == 'true' ) habits[i].classList.toggle( 'strike' )
